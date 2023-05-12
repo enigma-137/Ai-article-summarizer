407c8cab8616286f3d021f9f880705c5675274fc
enigma-devhub <emmanuelonoja291@gmail.com>
2023-05-12 07:56:54 +0100

    commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import logo from "../assets/mainlogo.png"
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="logo" className="w-48 object-contain" />
+        
+        <button 
+        type="button"
+        onClick={() => window.open('https://twitter.com')}
+        className="black_btn font-bold"
+        >
+  Twitter
+        </button>
+      </nav>
+      <h1 className="head_text text-tertiary">Summarize Your Articles with <br className="max-md:hidden" /> <span className="orange_gradient">OpenAI GPT-4</span>
+       </h1>
+       <h2 className="desc">
+        Tired of reading lengthy articles? Use the <span className="text-tertiary font-bold">Enigma AI Summarizer</span>  to transform long boring articles into clear and consise summaries  
+       </h2>
+    </header>
+  )
+}
+
+export default Hero 
